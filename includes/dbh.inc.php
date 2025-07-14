b155c53bf9882456cd92c90203cd26ff3dbf0482
Mark Aliño <markbrazal915@gmail.com>
2025-07-15 03:19:44 +0800

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,6 +4,7 @@ $dbname = 'mariaspostest'; // Ensure this is the correct database name
 $dbusername = 'root';
 $dbpassword = 'sBvjmATcWGBafojZaynYnauJPFWcAUYK';
 $port = "3306"; 
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
